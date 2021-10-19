c93ef405e49ec40815b6b6af03b4001db16190e4
Yousaf Arshad <yousaf.bsse1735@gmail.com>
2021-10-19 16:58:50 +0500

    Multiple class with same function

--- index.php
+++ index.php
@@ -1,13 +1,37 @@
 <?php
 
+class python {
+    private $name = 'python';
+    
+    public function intro () {
+        echo "This is " . $this->name . " language.<br/>";
+    }
+}
+
+class javascript {
+    private $name = 'javascript';
+    
+    public function intro () {
+        echo "This is " . $this->name . " language.<br/>";
+    }
+}
+
 class php {
 
     private $name = 'php';
     
     public function intro () {
-        echo "This is " . $this->name . " language";
+        echo "This is " . $this->name . " language.<br/>";
     }
+
 }
+
+$python_obj = new python();
+$javascript_obj = new javascript();
 $php_obj = new php();
+
+$python_obj->intro();
+$javascript_obj->intro();
 $php_obj->intro();
+
 ?>
