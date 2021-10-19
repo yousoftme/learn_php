9df5e8156e588ba15dd48844ce6d7b228b9e27cf
Yousaf Arshad <yousaf.bsse1735@gmail.com>
2021-10-19 17:16:09 +0500

    Php Inheritance

--- index.php
+++ index.php
@@ -1,34 +1,29 @@
 <?php
 
-class python {
-    private $name = 'python';
-    
+class language {
+    protected $name;
+    public function __construct($name) {
+        $this->name = $name;
+    }
     public function intro () {
         echo "This is " . $this->name . " language.<br/>";
     }
 }
-
-class javascript {
-    private $name = 'javascript';
-    
-    public function intro () {
-        echo "This is " . $this->name . " language.<br/>";
-    }
-}
-
-class php {
-
-    private $name = 'php';
-    
-    public function intro () {
-        echo "This is " . $this->name . " language.<br/>";
-    }
+class python extends language {
 
 }
 
-$python_obj = new python();
-$javascript_obj = new javascript();
-$php_obj = new php();
+class javascript extends language {
+
+}
+
+class php extends language {
+
+}
+
+$python_obj = new python('python');
+$javascript_obj = new javascript('javascript');
+$php_obj = new php('php');
 
 $python_obj->intro();
 $javascript_obj->intro();
