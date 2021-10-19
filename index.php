8d3215777e67bd7932429bf647815104670e245f
Yousaf Arshad <yousaf.bsse1735@gmail.com>
2021-10-19 16:52:13 +0500

    basic intro class

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-echo 'My name is yousaf';
+
+class php {
+
+    private $name = 'php';
+    
+    public function intro () {
+        echo "This is " . $this->name . " language";
+    }
+}
+$php_obj = new php();
+$php_obj->intro();
+?>
