13033836ccb0442445a2865237024cb8e4411ff2
Yousaf Arshad <yousaf.bsse1735@gmail.com>
2021-10-19 18:21:02 +0500

    php abstraction

--- index.php
+++ index.php
@@ -1,24 +1,38 @@
 <?php
 
-class language {
+abstract class Language {
     protected $name;
     public function __construct($name) {
         $this->name = $name;
     }
+
     public function intro () {
         echo "This is " . $this->name . " language.<br/>";
     }
-}
-class python extends language {
+
+    abstract protected function howToPrint();
 
 }
 
-class javascript extends language {
-
+class python extends Language {
+    public function howToPrint()
+    {
+        echo 'print("This is how to print in Python")<br/>';
+    }
 }
 
-class php extends language {
+class javascript extends Language {
+    public function howToPrint()
+    {
+        echo 'document.getElementById("demo").innerHTML = "This is how to print in Javascript"<br/>';
+    }
+}
 
+class php extends Language {
+    public function howToPrint()
+    {
+        echo 'echo "This is how to print in Python"<br/>';
+    }
 }
 
 $python_obj = new python('python');
@@ -26,7 +40,15 @@ $javascript_obj = new javascript('javascript');
 $php_obj = new php('php');
 
 $python_obj->intro();
+$python_obj->howToPrint();
+echo '<br />';
+
 $javascript_obj->intro();
+$javascript_obj->howToPrint();
+echo '<br />';
+
 $php_obj->intro();
+$php_obj->howToPrint();
+echo '<br />';
 
 ?>
