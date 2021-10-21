8d0680f11060ecec0b1b3c5cede631d4da4fb978
Yousaf Arshad <yousaf.bsse1735@gmail.com>
2021-10-21 18:51:50 +0500

    php magic function & function overloading

--- index.php
+++ index.php
@@ -20,6 +20,8 @@ interface Variable {
 
 abstract class Language {
     protected $name;
+    abstract protected function howToPrint();
+
     public function __construct($name) {
         $this->name = $name;
     }
@@ -28,8 +30,95 @@ abstract class Language {
         echo "This is " . $this->name . " language.<br/>";
     }
 
-    abstract protected function howToPrint();
+    public function __call($name, $arguments)
+    {
+        if ($name == 'howToUseIterators') {
+            if (in_array('for', $arguments)) {
+                echo '<pre><code>
+                        for ($x = 0; $x <= 10; $x++) {
+                            echo "The number is: $x <br>";
+                        } 
+                    </code></pre>';
+            }
+            if (in_array('foreach', $arguments)) {
+                echo '<pre><code>
+                        $colors = array("red", "green", "blue", "yellow");
 
+                        foreach ($colors as $value) {
+                            echo "$value <br>";
+                        }
+                    </code></pre>';
+            }
+            if (in_array('while', $arguments)) {
+                echo '<pre><code>
+                        $x = 0;
+
+                        while($x <= 100) {
+                            echo "The number is: $x <br>";
+                            $x+=10;
+                        } 
+                    </code></pre>';
+            }
+            if (in_array('dowhile', $arguments)) {
+                echo '<pre><code>
+                        $x = 1;
+
+                        do {
+                            echo "The number is: $x <br>";
+                            $x++;
+                        } while ($x <= 5);
+                    </code></pre>';
+            }
+        } elseif ($name == 'howToUseConditions') {
+            if ($arguments[0] == 'python') {
+                if (in_array('ifelseif', $arguments)) {
+                    echo '<pre><code>
+                    a = 200
+                    b = 33
+                    if b > a:
+                      print("b is greater than a")
+                    elif a == b:
+                      print("a and b are equal")
+                    else:
+                      print("a is greater than b")
+                        </code></pre>';
+                }
+            } else {
+                if (in_array('ifelseif', $arguments)) {
+                    echo '<pre><code>
+                            $t = date("H");
+    
+                            if ($t < "10") {
+                                echo "Have a good morning!";
+                            } elseif ($t < "20") {
+                                echo "Have a good day!";
+                            } else {
+                                echo "Have a good night!";
+                            }
+                        </code></pre>';
+                }
+                if (in_array('switch', $arguments)) {
+                    echo '<pre><code>
+                            $favcolor = "red";
+    
+                            switch ($favcolor) {
+                            case "red":
+                                echo "Your favorite color is red!";
+                                break;
+                            case "blue":
+                                echo "Your favorite color is blue!";
+                                break;
+                            case "green":
+                                echo "Your favorite color is green!";
+                                break;
+                            default:
+                                echo "Your favorite color is neither red, blue, nor green!";
+                            }
+                        </code></pre>';
+                }
+            }
+        }
+    }
 }
 
 class python extends Language implements Variable {
@@ -95,6 +184,7 @@ $python_obj->intro();
 $python_obj->howToPrint();
 $python_obj->howToComment();
 $python_obj->checkType();
+$python_obj->howToUseConditions('python', 'ifelseif');
 echo '<br />';
 
 $javascript_obj->intro();
@@ -107,6 +197,7 @@ $php_obj->intro();
 $php_obj->howToPrint();
 $php_obj->howToComment();
 $php_obj->checkType();
+$php_obj->howToUseIterators('for', 'while');
 echo '<br />';
 
 ?>
